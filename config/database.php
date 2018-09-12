b827525e8fe77d48f75b24955a9c660368f2cfd3
Alex <mashkovtsev.lx@gmail.com>
2018-09-12 19:48:25 +0300

    security :)

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-    $DB_DSN = 'mysql:host=db.tzk607.nic.ua;dbname=kittygra_kit';
+    $DB_DSN = 'mysql:host=RESTRICTED;dbname=RESTRICTED';
     $DB_USER = 'kittygra';
     $DB_PASSWORD = 'haqs&f0elbq';
 ?>
